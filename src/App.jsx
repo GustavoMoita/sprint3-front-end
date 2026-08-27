ea255b93b416105926007e641410d237f72b397e
Moita <gustavomoitadelima@gmail.com>
2026-08-27 13:12:02 -0300

    funcionalidade: montar narrativa completa e semântica da landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import Audience from './components/Audience'
+import CameraDemo from './components/CameraDemo'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Gallery from './components/Gallery'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Impact from './components/Impact'
+import Problem from './components/Problem'
+import Solution from './components/Solution'
+import Team from './components/Team'
+
 export default function App() {
   return (
-    <main id="conteudo">
-      <h1>Humanly × JOVI</h1>
-    </main>
+    <>
+      <a className="skip-link" href="#conteudo">Pular para o conteúdo</a>
+      <Header />
+      <main id="conteudo">
+        <Hero />
+        <Problem />
+        <Solution />
+        <CameraDemo />
+        <Audience />
+        <Gallery />
+        <Impact />
+        <Team />
+        <Contact />
+      </main>
+      <Footer />
+    </>
   )
 }
